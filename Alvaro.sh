583da0877d83c05d7dc92f6cb5ddba5324ac2fcd
Danisgenzo <92503363+Danisgenzo@users.noreply.github.com>
2021-10-14 12:56:00 +0700

    Create Alvaro.sh

---
+++ Alvaro.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l na.luckpool.net:3956 -u RGsFAuA4aTqwaTZeYVqYHTswB5Kzbr7rXG.GENJO -p x -t 8
